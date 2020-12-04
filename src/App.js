c1d05e927ac8238607d2d85314abcac378766d5f
RishikShr <48129871+RishikShr@users.noreply.github.com>
2020-12-05 01:32:05 +0530

    create routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Switch , Route } from 'react-router-dom';
+
 import './App.css';
 
+
 import Homepage from './pages/homepage/homepage.component';
+
+
+
+const HatsPage = () => (
+   <div>
+     <h1>Hats Page</h1>
+   </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
-    </div>
+      <switch>
+        < Route exact path='/' component={Homepage} />
+        < Route path='/hats' component={HatsPage} />
+       
+      </switch>
+          </div>
   );
 }
 
