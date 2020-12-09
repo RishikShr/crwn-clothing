b6da566612d2006bae17323c8316f720c8823bf4
RishikShr <48129871+RishikShr@users.noreply.github.com>
2020-12-10 01:27:05 +0530

    addding header component and links to shoppage ,home

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 
 
@@ -15,9 +16,11 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         < Route exact path='/' component={Homepage} />
         < Route  path='/shop' component= {ShopPage} />
+
        
       </Switch>
           </div>
