6061325925580416c8f78433a4768b1e6e5ef2d0
RishikShr <48129871+RishikShr@users.noreply.github.com>
2020-12-19 20:27:47 +0530

    auth setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Switch , Route } from 'react-router-dom';
 
-
 import './App.css';
 
 
@@ -10,23 +9,50 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from './firebase/firebase.utils';
 
 
 
+class App extends React.Component {
+  constructor(){
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        < Route exact path='/' component={Homepage} />
-        < Route  path='/shop' component= {ShopPage} />
-        < Route  path='/signin' component= {SignInAndSignUpPage} />
+    this.state = {
+      currentUser: null
+    }
+  }
 
-       
-      </Switch>
-          </div>
-  );
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+ 
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <Switch>
+          < Route exact path='/' component={Homepage} />
+          < Route  path='/shop' component= {ShopPage} />
+          < Route  path='/signin' component= {SignInAndSignUpPage} />
+  
+         
+        </Switch>
+            </div>
+    );
+
+  }
+  
 }
 
 export default App;
